a54975a2fd7c64eb489d114476d4ec4274b084f4
ibukiyabu <ibuki.y2115@gmail.com>
2024-02-19 19:09:47 +0900

    DBConnector作成〜記述

---
+++ src/com/diworksdev/webproj5/util/DBConnector.java
@@ -0,0 +1,39 @@
+package com.diworksdev.webproj5.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnector {
+	/**
+	 * JDBCドライバー名
+	 */
+	private static String driverName = "com.mysql.jdbc.Driver";
+	
+	/**
+	 * データベース接続URL
+	 */
+	private static String url = "jdbc:mysql://localhost/testdb";
+	/**
+	 * データ接続ユーザー名
+	 */
+	private static String user = "root";
+	/**
+	 * データ接続パスワード
+	 */
+	private static String passsword = "root";
+	
+	public Connection getcConnection() {
+		Connection con = null;
+		
+		try {
+			Class.forName(driverName);
+			con = DriverManager.getConnection(url, user, passsword);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch(SQLException e) {
+			e.printStackTrace();
+		}
+		return con;
+	}
+}
